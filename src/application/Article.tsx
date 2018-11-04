78c02732ad4b4df108ce2c6f1acd24894a88447e
Sammyt <sammy.teillet@gmail.com>
2018-11-04 19:10:06 +0100

    feat(): article page

--- src/application/Article.tsx
+++ src/application/Article.tsx
@@ -1,7 +1,40 @@
+import gql from "graphql-tag";
 import * as React from "react";
+import { Query } from "react-apollo";
+import * as ReactMarkdown from "react-markdown";
+import { match } from "react-router";
 
-export class Article extends React.PureComponent {
+const articleQuery = gql`
+  query GetArticleById($articleId: Int!) {
+    article(id: $articleId) {
+      id: id
+      name: name
+      content: content
+    }
+  }
+`;
+
+interface IProps {
+  match: match<{ id: string }>;
+}
+export class Article extends React.PureComponent<IProps> {
   public render() {
-    return <h2>Article page</h2>;
+    return (
+      <Query
+        query={articleQuery}
+        variables={{ articleId: +this.props.match.params.id }}
+      >
+        {({ loading, error, data }) => {
+          if (loading) {
+            return <p>Loading...</p>;
+          }
+          if (error) {
+            return <p>Error :(</p>;
+          }
+
+          return <ReactMarkdown source={data.article.content} />;
+        }}
+      </Query>
+    );
   }
 }
--- src/application/ArticleList.tsx
+++ src/application/ArticleList.tsx
@@ -5,15 +5,16 @@ import { Link, match } from "react-router-dom";
 import "./ArticleList.css";
 
 interface IArticle {
-  articleName: string;
-  articleId: number;
+  id: number;
+  name: string;
 }
 
-const articleQuery = gql`
+const articlesQuery = gql`
   {
     articles {
-      articleId: id
-      articleName: name
+      id: id
+      name: name
+      content: content
     }
   }
 `;
@@ -26,7 +27,7 @@ export class ArticleList extends React.PureComponent<IProps> {
   public render() {
     return (
       <div className="main-content-wrap">
-        <Query query={articleQuery}>
+        <Query query={articlesQuery}>
           {({ loading, error, data }) => {
             if (loading) {
               return <p>Loading...</p>;
@@ -35,13 +36,13 @@ export class ArticleList extends React.PureComponent<IProps> {
               return <p>Error :(</p>;
             }
 
-            return data.articles.map(({ articleName, articleId }: IArticle) => (
-              <div className="Article-container" key={articleId}>
+            return data.articles.map(({ name, id }: IArticle) => (
+              <div className="Article-container" key={id}>
                 <Link
                   className="Article-link"
-                  to={`${this.props.match.url}/${articleId}`}
+                  to={`${this.props.match.url}/${id}`}
                 >
-                  <h3>{`${articleName}`}</h3>
+                  <h3>{`${name}`}</h3>
                 </Link>
                 <div>
                   <time className="time-metadata"> November 8, 2018 </time>
