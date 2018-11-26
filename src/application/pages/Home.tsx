1387774036152b476ffebbfb77b523a500eb20a7
Sammyt <sammy.teillet@gmail.com>
2018-11-26 23:29:21 +0100

    feat(): Home page redirects on article list

--- src/application/pages/Home.tsx
+++ src/application/pages/Home.tsx
@@ -1,13 +1,9 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import "./Home.css";
 
 export class Home extends React.PureComponent {
   public render() {
-    return (
-      <Link className="Home" to="/articles">
-        <h1 className="App-title">Go to blog</h1>
-      </Link>
-    );
+    return <Redirect to="/articles" />;
   }
 }
