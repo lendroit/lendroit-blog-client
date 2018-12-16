c1097eb3c0a93b3aa5a1dde001d980f8af9e688a
Sammyt <sammy.teillet@gmail.com>
2018-12-16 16:14:35 +0100

    style(): Fix mobile article list

--- src/application/ArticleList.tsx
+++ src/application/ArticleList.tsx
@@ -1,6 +1,7 @@
 import gql from "graphql-tag";
 import * as React from "react";
 import { Query } from "react-apollo";
+import { BrowserView, MobileView } from "react-device-detect";
 import { Link, match } from "react-router-dom";
 import { getDisplayDate } from "../utils/getDisplayDate";
 import { getRandomColor } from "../utils/getRandomColor";
@@ -45,30 +46,79 @@ export class ArticleList extends React.PureComponent<IProps> {
             ({ name, id, createdAt }: IArticle, index: number) => {
               const textColor = getRandomColor();
               return (
-                <Link
-                  key={id}
-                  className="Article-container"
-                  style={{
-                    backgroundColor: getRandomColor(),
-                    flexDirection: index % 2 ? "row" : "row-reverse"
-                  }}
-                  to={`${this.props.match.url}/${id}`}
-                >
-                  <div className="Article-semi-container">
-                    <span
-                      style={{ fontSize: "10em", color: textColor }}
-                      className={getRandomIcon()}
-                    />
-                  </div>
-                  <div className="Article-semi-container">
-                    <div className="Article-name" style={{ color: textColor }}>
-                      <span>{`${name}`}</span>
-                    </div>
-                    <time className="time-metadata" style={{ display: "none" }}>
-                      {getDisplayDate(createdAt)}
-                    </time>
-                  </div>
-                </Link>
+                <div key="croute">
+                  <BrowserView>
+                    <Link
+                      key={id}
+                      className="Article-container"
+                      style={{
+                        backgroundColor: getRandomColor(),
+                        flexDirection: index % 2 ? "row" : "row-reverse",
+                        height: 400
+                      }}
+                      to={`${this.props.match.url}/${id}`}
+                    >
+                      <div className="Article-semi-container">
+                        <span
+                          style={{ fontSize: "10em", color: textColor }}
+                          className={getRandomIcon()}
+                        />
+                      </div>
+                      <div className="Article-semi-container">
+                        <div
+                          className="Article-name"
+                          style={{ color: textColor }}
+                        >
+                          <span>{`${name}`}</span>
+                        </div>
+                        <time
+                          className="time-metadata"
+                          style={{ display: "none" }}
+                        >
+                          {getDisplayDate(createdAt)}
+                        </time>
+                      </div>
+                    </Link>
+                  </BrowserView>
+                  <MobileView>
+                    <Link
+                      key={id}
+                      className="Article-container"
+                      style={{
+                        backgroundColor: getRandomColor(),
+                        flexDirection: index % 2 ? "row" : "row-reverse"
+                      }}
+                      to={`${this.props.match.url}/${id}`}
+                    >
+                      <div>
+                        <div
+                          className="Article-name-mobile"
+                          style={{
+                            color: textColor,
+                            fontSize: "3em",
+                            textAlign: "center"
+                          }}
+                        >
+                          <span>{`${name}`}</span>
+                        </div>
+                        <div
+                          style={{
+                            color: textColor,
+                            fontSize: "4em",
+                            textAlign: "center"
+                          }}
+                          className={getRandomIcon()}
+                        />
+                        <time
+                          className="time-metadata"
+                          style={{ display: "none" }}
+                        >
+                          {getDisplayDate(createdAt)}
+                        </time>
+                      </div>
+                    </Link>
+                  </MobileView>
+                </div>
               );
             }
           );
